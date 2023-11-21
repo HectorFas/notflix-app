9b47f422db60ac70d8125d7944ba84a9275c2d54
Hector Fas <hectorcaballerofas.hcf@gmail.com>
2023-11-21 16:32:28 +0100

    .

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -1,20 +1,46 @@
 <x-app-layout>
+    <div style="float: right; margin: 10px;">
         <form action="/films/search" method="GET">
-            <label for="search">Search:</label>
-            <input type="text"  name="search" required>
-            <button type="submit">Search</button>
+            <label for="search" style="font-size: 14px;">Search:</label>
+            <input type="text" name="search" required style="width: 150px; height: 25px; font-size: 14px;">
+            <button type="submit" style="height: 25px;">Search</button>
         </form>
+    </div>
 
-        <p>Estas son tus peliculas</p>
+    <div class="film-container">
         @forelse ($films as $film)
-            <div>
+            <div class="film-item">
                 <p>{{$film->title}}</p>
-                <img src={{$film->image}} width=100/>
+                <div class="image-container">
+                    <img src="{{$film->image}}" alt="{{$film->title}}" style="max-width: 100%; max-height: 100%;">
+                </div>
                 <livewire:counter :filmid="$film->id"/>
             </div>
-
         @empty
             <p>Tenemos un problema con el servidor y no hay peliculas</p>
         @endforelse
-
+    </div>
 </x-app-layout>
+
+<style>
+    .film-container {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 20px; /* Ajusta este valor según tus necesidades de espacio entre las películas */
+        justify-content: center; /* Centra los elementos en el eje horizontal */
+    }
+
+    .film-item {
+        flex: 1 0 calc(25% - 20px); /* 25% para que haya 4 elementos por fila */
+        box-sizing: border-box;
+        text-align: center;
+    }
+
+    .image-container {
+        display: flex;
+        justify-content: center; /* Centra la imagen en el contenedor */
+    }
+</style>
+
+
+
